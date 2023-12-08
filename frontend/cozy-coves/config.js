728727474a4114af8405f0ef6411f52b768783d7
Nirupama Dissanayake <84626251+Niru-D@users.noreply.github.com>
2023-12-08 10:18:17 +0530

    Delete Config.js File

--- frontend/cozy-coves/config.js
+++
@@ -1,6 +0,0 @@
-window.config = {
-    redirectUrl: "https://a779adf4-cdde-462f-82c7-2ce991985eb1.e1-eu-north-azure.choreoapps.dev",
-    asgardeoClientId: "arCa9s8pb1rw6xw54SIst6IiRRUa",
-    asgardeoBaseUrl: "https://api.asgardeo.io/t/cozycoves",
-    choreoApiUrl: ""
-};
