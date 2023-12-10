1d071b3a79dc493790efed056fbbd0e5447f38a1
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-10 09:36:57 +0530

    Remove config.js

--- frontend/cozy-coves/config.js
+++
@@ -1,6 +0,0 @@
-window.config = {
-    redirectUrl: "http://localhost:3000",
-    asgardeoClientId: "arCa9s8pb1rw6xw54SIst6IiRRUa",
-    asgardeoBaseUrl: "https://api.asgardeo.io/t/cozycoves",
-    choreoApiUrl: ""
-};
