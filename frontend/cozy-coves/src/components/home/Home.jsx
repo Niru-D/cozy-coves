e29c385c7f40ecbeeaace325fb5a0f9c532b4b74
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-10 11:49:27 +0530

    Add Testing Changes in Image Paths

--- frontend/cozy-coves/src/components/home/Home.jsx
+++ frontend/cozy-coves/src/components/home/Home.jsx
@@ -4,9 +4,14 @@ import { useAuthContext } from '@asgardeo/auth-react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-// import home1 from 'src\assets\home1.jpg';
+import home1 from '../../assets/home1.jpg';
 import home2 from '../../assets/home2.jpg';
-// import home3 from 'src/assets/home3.jpg';
+import home3 from '../../assets/home3.jpg';
+import home4 from '../../assets/home4.jpg';
+import home5 from '../../assets/home5.jpg';
+import home6 from '../../assets/home6.jpg';
+import home7 from '../../assets/home7.jpg';
+import home8 from '../../assets/home8.jpg';
 
 const Home = () => {
   const { state, signIn, signOut } = useAuthContext();
@@ -26,28 +31,28 @@ const Home = () => {
     <div className="home">
       <Slider {...settings}>
         <div>
-          <img src="src/assets/home1.jpg" alt="Slide 1" />
+          <img src={home1} alt="Slide 1" />
         </div>
         <div>
           <img src={home2} alt="Slide 2" />
         </div>
         <div>
-          <img src="src\assets\home3.jpg" alt="Slide 3" />
+          <img src={home3} alt="Slide 3" />
         </div>
         <div>
-          <img src="src\assets\home4.jpg" alt="Slide 4" />
+          <img src={home4} alt="Slide 4" />
         </div>
         <div>
-          <img src="src\assets\home5.jpeg" alt="Slide 5" />
+          <img src={home5} alt="Slide 5" />
         </div>
         <div>
-          <img src="src\assets\home6.jpg" alt="Slide 6" />
+          <img src={home6} alt="Slide 6" />
         </div>
         <div>
-          <img src="src\assets\home7.jpg" alt="Slide 7" />
+          <img src={home7} alt="Slide 7" />
         </div>
         <div>
-          <img src="src\assets\home8.jpg" alt="Slide 8" />
+          <img src={home8} alt="Slide 8" />
         </div>
         
       </Slider>
