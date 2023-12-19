2c6996f2a8c926fffc1e004a7a18868392fa9c8e
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-19 13:35:13 +0530

    Add Axios BaseURL

--- backend/cozycoves/src/main/java/com/cozycovesnyx/cozycoves/config/CorsConfig.java
+++ backend/cozycoves/src/main/java/com/cozycovesnyx/cozycoves/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
-//        config.addAllowedOrigin("https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1.0");
+//        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
