2ca308bd10788110effb6cfa9e2fb71ccbac53a7
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-22 05:00:43 +0530

    Remove Config File

--- frontend/cozy-coves/src/App.jsx
+++ frontend/cozy-coves/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/header/header';
-import Home from './components/home/home';
-import DashboardAdmin from './components/dashboard/admin/dashboardAdmin';
-import DashboardRenter from './components/dashboard/renter/dashboardRenter';
-import DashboardHouseOwner from './components/dashboard/houseOwner/dashboardHouseOwner';
+import Header from './components/header/header.jsx';
+import Home from './components/home/home.jsx';
+import DashboardAdmin from './components/dashboard/admin/dashboardAdmin.jsx';
+import DashboardRenter from './components/dashboard/renter/dashboardRenter.jsx';
+import DashboardHouseOwner from './components/dashboard/houseOwner/dashboardHouseOwner.jsx';
 import { useAuthContext } from '@asgardeo/auth-react';
 import { ChakraProvider } from '@chakra-ui/react'
 
