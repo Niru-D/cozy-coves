e092ca2265aafea9aba62ef82d02c09e2aef05da
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-22 05:41:38 +0530

    Change axiosConfig.js

--- frontend/cozy-coves/src/api/axiosConfig.js
+++ frontend/cozy-coves/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: window.config.axiosBaseUrl,
+    baseURL: window.config.choreoApiUrl,  
+    // Have to change locally   
     headers: {
         "ngrok-skip-browser-warning":"true",
         // "Access-Control-Allow-Origin": "*",
