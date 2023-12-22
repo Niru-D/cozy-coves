759e072c7a4e539f1013eb6ef64e7e65a07d810e
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-22 11:30:10 +0530

    Add Changes to axiosConfig

--- frontend/cozy-coves/src/api/axiosConfig.js
+++ frontend/cozy-coves/src/api/axiosConfig.js
@@ -23,25 +23,37 @@
 // });
 
 import axios from 'axios';
-import { useAuthContext } from '@asgardeo/auth-react';
-const { getAccessToken } = useAuthContext();
+import { useEffect } from 'react';
 
-const axiosInstance = axios.create({
-    baseURL: "https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1",
-    headers: {
-        "ngrok-skip-browser-warning": "true",
-        "Content-Type": "application/json" 
-    }
-});
+const useAuthContextAndCreateAxiosInstance = () => {
+    const { getAccessToken } = useAuthContext();
 
-useEffect(() => {
-    getAccessToken().then((accessToken) => {
-        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-        console.log("accessToken");
-        console.log(accessToken);
-    }).catch((error) => {
-        console.log(error);
-    });
-}, []);
+    useEffect(() => {
+        const createAxiosInstance = async () => {
+            const axiosInstance = axios.create({
+                baseURL: "https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1",
+                headers: {
+                    "ngrok-skip-browser-warning": "true",
+                    "Content-Type": "application/json"
+                }
+            });
 
-export default axiosInstance;
+            try {
+                const accessToken = await getAccessToken();
+                axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+                console.log("accessToken:", accessToken);
+            } catch (error) {
+                console.log("Error fetching access token:", error);
+                // Handle error if necessary
+            }
+        };
+
+        createAxiosInstance();
+    }, [getAccessToken]);
+
+    // Optionally return axiosInstance or use it inside the useEffect
+};
+
+export default useAuthContextAndCreateAxiosInstance;
+
+
