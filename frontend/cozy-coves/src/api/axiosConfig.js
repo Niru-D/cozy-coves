7fcf3e2492e45baedc48c48d12066131cc3d1e99
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-22 09:35:59 +0530

    Add Access Token to axiosConfig

--- frontend/cozy-coves/src/api/axiosConfig.js
+++ frontend/cozy-coves/src/api/axiosConfig.js
@@ -1,10 +1,46 @@
-import axios from 'axios';
+// import axios from 'axios';
+// const { getAccessToken } = useAuthContext();
 
-export default axios.create({
-    baseURL: "https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1", 
-    // baseURL: window.config.axiosBaseUrl,
+// useEffect(() => {
+//     getAccessToken().then((accessToken) => {
+//         console.log("accessToken");
+//         console.log(accessToken);
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+// }, []);
+
+
+// export default axios.create({
+//     baseURL: "https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1", 
+//     // baseURL: window.config.axiosBaseUrl,
+//     headers: {
+//         "ngrok-skip-browser-warning":"true",
+//         // "Access-Control-Allow-Origin": "*",
+//         "Authorization": "Bearer" + accessToken,
+//         "Content-Type":"Application/Json"
+//     }
+// });
+
+import axios from 'axios';
+const { getAccessToken } = useAuthContext();
+
+const axiosInstance = axios.create({
+    baseURL: "https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1",
     headers: {
-        "ngrok-skip-browser-warning":"true",
-        // "Access-Control-Allow-Origin": "*",
+        "ngrok-skip-browser-warning": "true",
+        "Content-Type": "application/json" 
     }
 });
+
+useEffect(() => {
+    getAccessToken().then((accessToken) => {
+        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        console.log("accessToken");
+        console.log(accessToken);
+    }).catch((error) => {
+        console.log(error);
+    });
+}, []);
+
+export default axiosInstance;
