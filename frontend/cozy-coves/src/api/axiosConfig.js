2420f1b9783fd9691c2029def929f3a1a264a147
Nirupama Dissanayake <nirupama@wso2.com>
2023-12-22 05:57:27 +0530

    Change axiosConfig.js

--- frontend/cozy-coves/src/api/axiosConfig.js
+++ frontend/cozy-coves/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: window.config.choreoApiUrl,  
+    baseURL: "https://3f434093-3f6a-40a5-a34b-ca372af6c39e-dev.e1-eu-north-azure.choreoapis.dev/grvi/housemanagementservice/cozycoves-rest-endpoint-5c6/v1.0", 
     // Have to change locally   
     headers: {
         "ngrok-skip-browser-warning":"true",
